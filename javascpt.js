183bbe457266270e7306f5fb917772975fb840fb
moraislaila <nataliacuchi27@gmail.com>
2025-06-30 11:04:28 -0300

    Add files via upload
    estrutura javascript for

---
+++ javascpt.js
@@ -0,0 +1,21 @@
+const valor = [3, 7, 18, 21, 33]
+
+    console.log(valor)
+
+    function inserirFinalArray(){
+        var novovalor = prompt("digite seu valor")
+        valor.push(novovalor)
+        console.log(valor)
+    }
+
+        function calcularmedia(){
+            var soma = 0 , media = 0 
+            for(i=0; i < numeros.length; i++){
+             soma += numeros[i];
+            }
+            media = soma / numeros.length
+            alert(media)
+            }
+
+
+            
